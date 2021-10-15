1e2774490a8438162368a50e9c6ffc5c1eb837a4
nknuecht <nknuechterlein@gmail.com>
2021-10-15 09:24:12 -0700

    utils update

--- utils/reformat_xena_example_data.py
+++ utils/reformat_xena_example_data.py
@@ -8,11 +8,11 @@ __maintainer__ = "Nicholas Nuechterlein"
 import pandas as pd
 
 # load tsv SCNA matrix of [genes x patients] and save as csv file of [patients x genes]
-scna_path = '../data/TCGA.GBMLGG.sampleMap%2FGistic2_CopyNumber_Gistic2_all_thresholded.by_genes.gz'
+scna_path = './data/TCGA.GBMLGG.sampleMap%2FGistic2_CopyNumber_Gistic2_all_thresholded.by_genes.gz'
 print('Loading Xena Data')
 scna_df = pd.read_csv(scna_path, sep='\t', index_col=0).T
 print('Saving reformatted Xena data of')
 print('Shape:\t', scna_df.shape)
 print('Patients:\t', scna_df.index[:3])
 print('Genes:\t', scna_df.columns[:3])
-scna_df.to_csv('../data/Xena_GBMLGG_GISTIC_Scores.csv')
+scna_df.to_csv('./data/Xena_GBMLGG_GISTIC_Scores.csv')
