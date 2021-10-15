de173bfdb96951b094e69d15b2ac4503fdca33bd
nknuecht <nknuechterlein@gmail.com>
2021-10-15 09:47:55 -0700

    utils update

--- utils/reformat_xena_example_data.py
+++ utils/reformat_xena_example_data.py
@@ -16,9 +16,6 @@ scna_df = pd.read_csv(scna_path, sep='\t', index_col=0).T
 
 # save SCNA matrix as csv file of [patients x genes]
 outfile = './data/Xena_GBMLGG_GISTIC_Scores.csv'
-print('Saving reformatted Xena data of')
+print('\nSaving reformatted Xena data of')
 print(' -> to', os.path.abspath(outfile))
-print('Shape:\t', scna_df.shape)
-print('Patients:\t', scna_df.index[:3])
-print('Genes:\t', scna_df.columns[:3])
 scna_df.to_csv(outfile)
